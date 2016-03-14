f991b63c949f1280220cbffafe94a23c3e64b4ae
krishnamohan.a <krishnamohan.a@vimukti.com>
2016-03-14 11:42:38 +0530

    Ecgine Download page added

--- _assets/javascripts/custom/custom.js
+++ _assets/javascripts/custom/custom.js
@@ -2,3 +2,25 @@
 * Custom Javascripts
 * If you prefer to use CoffeeScript, rename this files to custom.coffee
 */
+<script type="text/javascript">
+function validateDownloadForm() {
+	var form = document.getElementById("downloadform");
+	var result=form.elements["osandtype"].value;
+    if (result==null || restult=="") {
+        alert("Please select an Ecgine setup appropriate for you system.");
+        return false;
+    }else{
+		switch (result) {
+			case "windows64":
+				window.open('http://s1.infra.ecgine.com/files/ecgine-client-x64-1.0.exe');
+				break;
+			case "windows32":
+				window.open('http://s1.infra.ecgine.com/files/ecgine-client-x86-1.0.exe');
+				break;
+			case "mac":
+				window.open('');
+				break;
+		}
+    }
+}
+</script>
